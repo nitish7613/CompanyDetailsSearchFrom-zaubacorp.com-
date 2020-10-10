bf972fd4f66311b09e9fe603f4e9679361bbadc7
nitish7613 <nitish7613@gmail.com>
2020-10-10 15:32:47 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+const MongoClient=require('mongodb').MongoClient;
+const assert=require('assert');
+var router = express.Router();
+
+const url1='mongodb://localhost:27017/';
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', { title: 'Express' });
+});
+
+router.post('/lookup', function(req, res){
+    var ani= [];
+	cname = (req.body.cname);
+	url = 'https://www.zaubacorp.com/companysearchresults/' + JSON.stringify(cname);
+	console.log(url);
+	request(url, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+			var title;
+
+    //var json = { title : "", release : "", rating : ""};
+
+	    if($('#results').length != 0){
+    	console.log("inif");
+    	$('#results').filter(function(){
+    	var data = $(this);
+    	console.log("here");
+    	url = data.find("a").first().attr("href");
+    	console.log("hahf");
+    	console.log(url);
+    	request(url, function(error, response, html){
+    		if(!error){
+    			var $ = cheerio.load(html);
+    			var table;
+    			$('html').filter(function(){
+    				var html = $(this);
+    				title = html.find('title').first().html();
+    				cin = (html.find('thead').first().html());
+    			})
+           
+   MongoClient.connect(url1, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myobj = { cname: cname, cin: cin };
+
+  dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+});
+ dbo.collection("customers").find({}, function(err, result) {
+    if (err) throw err;
+    result.forEach((doc,err)=>{
+        assert.equal(null,err);
+        var abs= "<tr><td>Company</td><td>"+doc.cname+"</td></tr>"+doc.cin;
+        ani.push(abs);
+        },()=>{
+            db.close();
+                res.render('data',{title: title,items:ani});
+        });
+    
+
+    
+});
+
+  
+});
+    
+       
+
+               console.log(table);
+    			
+    		}            
+    	})
+       // json.title = title;
+       // json.release = release;
+   })
+    }
+    else
+    	res.status(404).send('Company Not Found!');
+
+    }
+})
+});
+
+	module.exports = router;
+
+
+      //<tr>
+        //  <td><p>Foreign Company Registration Number</p></td>
+          //<td><p><a href="https://www.zaubacorp.com/company/TATA-LIMITED/F02452">F02452</a></p></td>
+        //</tr>
